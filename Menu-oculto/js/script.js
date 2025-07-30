a92f8c6886d1df8d553018614b2031166c6eaaa8
elmejorbruno <brunoponceprop@gmail.com>
2025-07-30 16:53:58 -0400

    Add files via upload
    pagina prueba

---
+++ Menu-oculto/js/script.js
@@ -0,0 +1,7 @@
+function menuOculto(){
+    const abrirMenu = document.querySelector('.abrir-menu');
+    const cambiarEstadoBanner = document.querySelector('.banner');
+    abrirMenu.classList.toggle('active');
+    cambiarEstadoBanner.classList.toggle('active');
+
+}
